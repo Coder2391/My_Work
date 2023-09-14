0aff2d87d80cab8405779dfb1b43e426aa8800c3
Yash Chauhan <97875322+Coder2391@users.noreply.github.com>
2023-09-14 19:10:49 +0530

    Add files via upload
    Day2 Task

---
+++ For_each_loop.java
@@ -0,0 +1,28 @@
+
+public class For_each_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] arr = new int[5];
+		
+		arr[0] = 30;
+		arr[1] = 50;
+		arr[2] = 25;
+		arr[3] = 70;
+		arr[4] = 10;
+		
+		int m = arr[0];
+		for(int k: arr) {
+			System.out.println(k + " ");
+				
+			if(k > m) {
+				m = k;	
+			}
+		}
+			System.out.println("The Highest num:" + m);
+			
+		
+	}
+
+}
